a7b642de85e11b9b8ee258ee6a74e66d96741744
gdanny94 <109629790+gdanny94@users.noreply.github.com>
2025-03-11 15:45:28 -0400

    updaded form

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,57 +57,57 @@ jQuery(document).ready(function($) {
     
     
     // AJAX CONTACT FORM SUBMIT
-    $("#contact-form").submit(function(e) {
+    // $("#contact-form").submit(function(e) {
 
-        e.preventDefault();
-        var postdata = $(this).serialize();
+    //     e.preventDefault();
+    //     var postdata = $(this).serialize();
 
-        $.ajax({
+    //     $.ajax({
 
-            type: "POST",
-            url: "assets/php/contact.php",
-            data: postdata,
-            dataType: "json",
-            success: function(json) {
+    //         type: "POST",
+    //         url: "assets/php/contact.php",
+    //         data: postdata,
+    //         dataType: "json",
+    //         success: function(json) {
 
-                $("#contact-form input, #contact-form textarea").removeClass("error");
+    //             $("#contact-form input, #contact-form textarea").removeClass("error");
 
-                setTimeout(function(){
+    //             setTimeout(function(){
 
-                    if (json.nameMessage !== "") {
+    //                 if (json.nameMessage !== "") {
 
-                        $("#contact-form-name").addClass("error");
+    //                     $("#contact-form-name").addClass("error");
 
-                    }
+    //                 }
 
-                    if (json.emailMessage !== "") {
+    //                 if (json.emailMessage !== "") {
 
-                        $("#contact-form-email").addClass("error");
+    //                     $("#contact-form-email").addClass("error");
 
-                    }
+    //                 }
 
-                    if (json.messageMessage !== "") {
+    //                 if (json.messageMessage !== "") {
 
-                        $("#contact-form-message").addClass("error");
+    //                     $("#contact-form-message").addClass("error");
 
-                    }
+    //                 }
 
-                }, 10);
+    //             }, 10);
 
-                if (json.nameMessage === "" && json.emailMessage === "" && json.messageMessage === "") {
+    //             if (json.nameMessage === "" && json.emailMessage === "" && json.messageMessage === "") {
 
-                    $("#contact-form.error input, #contact-form.error textarea").removeClass("error");
-                    $('#contact-form').addClass("success");
-                    $('#contact-form textarea, #contact-form input').attr("placeholder","");
-                    $('#contact-form input, #contact-form button, #contact-form textarea').val('').prop('disabled', true);
+    //                 $("#contact-form.error input, #contact-form.error textarea").removeClass("error");
+    //                 $('#contact-form').addClass("success");
+    //                 $('#contact-form textarea, #contact-form input').attr("placeholder","");
+    //                 $('#contact-form input, #contact-form button, #contact-form textarea').val('').prop('disabled', true);
 
-                }
+    //             }
 
-            }
+    //         }
 
-        });
+    //     });
 
-    });
+    // });
 
     
 });
